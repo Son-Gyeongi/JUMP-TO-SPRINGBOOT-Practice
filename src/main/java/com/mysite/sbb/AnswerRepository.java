f197cb2ef2e9334305037f4520879b9039995313
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-03 01:46:41 +0900

    QuestionRepository, AnswerRepository create

---
+++ src/main/java/com/mysite/sbb/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * AnswerRepository를 이용하여 answer 테이블에 데이터를 저장하거나 조회할 수 있다.
+ */
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
---
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * 리포지터리는 엔티티에 의해 생성된 데이터베이스 테이블에
+ * 접근하는 메서드들(예: findAll, save 등)을 사용하기 위한 인터페이스
+ *
+ * QuestionRepository를 이용하여 question테이블에 데이터를 저장하거나 조회할 수 있다.
+ */
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    // findBySubject, Subject값으로 데이터 조회
+    Question findBySubject(String subject);
+
+    // findBySubjectAndContent, 제목과 내용을 함께 조회
+    Question findBySubjectAndContent(String subject, String content);
+
+    // findBySubjectLike, 제목에 특정 문자열이 포함되어 있는 데이터를 조회
+    List<Question> findBySubjectLike(String subject);
+}
