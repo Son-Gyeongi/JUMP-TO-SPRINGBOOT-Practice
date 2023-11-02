63a5616f8e104de389990b5f2d2184fc046fdd43
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-02 16:55:20 +0900

    sbb start

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -3,7 +3,10 @@ package com.mysite.sbb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * 프로그램 시작을 담당하는 파일
+ */
+@SpringBootApplication // 스프링부트의 모든 설정이 관리된다.
 public class SbbApplication {
 
     public static void main(String[] args) {
