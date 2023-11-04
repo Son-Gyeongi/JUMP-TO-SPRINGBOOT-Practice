548a42bd2c8c003cbf692e1a2a04dcc459559d98
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-04 15:56:48 +0900

    Question detail page

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
@@ -25,11 +28,18 @@ public class QuestionController {
      */
 
     // 데이터 조회하여 템플릿에 전달하기
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) { // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할
 //        List<Question> questionList = this.questionRepository.findAll();
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 컨트롤러에서 리포지터리를 직접 호출하지 않고 중간에 서비스(Service)를 두어 데이터를 처리한다.
@@ -19,5 +21,18 @@ public class QuestionService {
     // 질문 목록을 조회하여 리턴
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    // 데이터의 실제 제목과 내용을 출력해 보자.
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        // Optional 객체이기 때문에 위와 같이 isPresent 메서드로
+        // 해당 데이터가 존재하는지 검사하는 로직이 필요
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
