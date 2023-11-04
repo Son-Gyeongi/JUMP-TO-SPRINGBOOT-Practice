32de14f0c4e802b23a82c2eee329babcb602abc1
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-05 02:22:33 +0900

    답변 등록

---
+++ src/main/java/com/mysite/sbb/answer/AnswerSrevice.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerSrevice {
+
+    private final AnswerRepository answerRepository;
+
+    // 답변 저장하기
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
