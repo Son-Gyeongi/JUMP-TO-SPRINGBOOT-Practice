3dc1f5d963c0cdff2bac50a31da6a663c96cf85d
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-14 20:09:53 +0900

    답변 수정

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -6,13 +6,16 @@ import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -48,5 +51,42 @@ public class AnswerController {
         // 답변을 저장한다.
         this.answerSrevice.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    // URL의 답변 아이디를 통해 조회한 답변 데이터의 "내용"을 AnswerForm 객체에 대입하여 answer_form.html 템플릿에서 사용
+    public String answerModify(AnswerForm answerForm,
+                               @PathVariable("id") Integer id,
+                               Principal principal) {
+        Answer answer = this.answerSrevice.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerForm.setContent(answer.getContent());
+        return "answer_form"; // 답변을 수정하기 위한 템플릿
+        // 답변 수정시 기존의 내용이 필요하므로 AnswerForm 객체에 조회한 값을 저장해야 한다.
+    }
+
+    // POST 방식의 답변 수정을 처리
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm,
+                               BindingResult bindingResult,
+                               @PathVariable("id") Integer id,
+                               Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+
+        Answer answer = this.answerSrevice.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        this.answerSrevice.modify(answer, answerForm.getContent());
+
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId()); // 질문 상세 페이지
     }
 }
--- src/main/java/com/mysite/sbb/answer/AnswerSrevice.java
+++ src/main/java/com/mysite/sbb/answer/AnswerSrevice.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,25 @@ public class AnswerSrevice {
         answer.setAuthor(author); // 답변 저장시 작성자를 저장
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    // 답변 조회
+    // 답변 아이디로 답변을 조회하는 getAnswer 메서드
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    // 답변 수정
+    // 답변의 내용으로 답변을 수정하는 modify 메서드
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
     }
 }
